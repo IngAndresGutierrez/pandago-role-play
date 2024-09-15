1a644208c8f376a6047c2707cf0736859d664678
Jaime Andres Gomez Gutierrez <g.gutierrez.j.andres@gmail.com>
2024-09-15 19:07:00 +0200

    feat: cache api memoization was added

--- src/modules/character-detail/services/character.ts
+++ src/modules/character-detail/services/character.ts
@@ -1,72 +1,79 @@
 import axios from 'axios'
+import { useMemoize } from '@vueuse/core'
+
 import { getQueryParamsApi } from '@/modules/common/utils/query-params'
 
 const BASE_URL = 'https://gateway.marvel.com/v1/public'
 
-export const getCharacterDetail = async (characterId: number) => {
-  const url = `${BASE_URL}/characters/${characterId}?${getQueryParamsApi()}`
+export const getCharacterDetail = useMemoize(
+  async (characterId: number) => {
+    const url = `${BASE_URL}/characters/${characterId}?${getQueryParamsApi()}`
 
-  try {
-    const response = await axios.get(url)
+    try {
+      const response = await axios.get(url)
 
-    return response.data.data.results[0]
-  } catch (error) {
-    console.error('Error fetching character details:', error)
-    throw error
-  }
-}
+      return response.data.data.results[0]
+    } catch (error) {
+      console.error('Error fetching character details:', error)
+      throw error
+    }
+  })
 
-export const getCharacterDetailComics = async (characterId: number) => {
-  const url = `${BASE_URL}/characters/${characterId}/comics?${getQueryParamsApi()}`
+export const getCharacterDetailComics = useMemoize(
+  async (characterId: number) => {
+    const url = `${BASE_URL}/characters/${characterId}/comics?${getQueryParamsApi()}`
 
-  try {
-    const response = await axios.get(url)
+    try {
+      const response = await axios.get(url)
 
-    return response.data.data.results
-  } catch (error) {
-    console.error('Error fetching character comics:', error)
-    throw error
-  }
-}
+      return response.data.data.results
+    } catch (error) {
+      console.error('Error fetching character comics:', error)
+      throw error
+    }
+  })
 
-export const getCharacterDetailSeries = async (characterId: number) => {
-  const url = `${BASE_URL}/characters/${characterId}/series?${getQueryParamsApi()}`
+export const getCharacterDetailSeries = useMemoize(
+  async (characterId: number) => {
+    const url = `${BASE_URL}/characters/${characterId}/series?${getQueryParamsApi()}`
 
-  try {
-    const response = await axios.get(url)
+    try {
+      const response = await axios.get(url)
 
-    return response.data.data.results
-  } catch (error) {
-    console.error('Error fetching character series:', error)
-    throw error
-  }
-}
+      return response.data.data.results
+    } catch (error) {
+      console.error('Error fetching character series:', error)
+      throw error
+    }
+  })
 
-export const getCharacterDetailEvents = async (characterId: number) => {
-  const url = `${BASE_URL}/characters/${characterId}/events?${getQueryParamsApi()}`
+export const getCharacterDetailEvents = useMemoize(
+  async (characterId: number) => {
+    const url = `${BASE_URL}/characters/${characterId}/events?${getQueryParamsApi()}`
 
-  try {
-    const response = await axios.get(url)
+    try {
+      const response = await axios.get(url)
 
-    return response.data.data.results
-  } catch (error) {
-    console.error('Error fetching character events:', error)
-    throw error
-  }
-}
+      return response.data.data.results
+    } catch (error) {
+      console.error('Error fetching character events:', error)
+      throw error
+    }
+  })
 
-export const getCharacterDetailStories = async (characterId: number) => {
-  const url = `${BASE_URL}/characters/${characterId}/stories?${getQueryParamsApi()}`
+export const getCharacterDetailStories = useMemoize(
+  async (characterId: number) => {
+    const url = `${BASE_URL}/characters/${characterId}/stories?${getQueryParamsApi()}`
 
-  try {
-    const response = await axios.get(url)
+    try {
+      const response = await axios.get(url)
 
-    return response.data.data.results
-  } catch (error) {
-    console.error('Error fetching character stories:', error)
-    throw error
-  }
-}
+      return response.data.data.results
+    } catch (error) {
+      console.error('Error fetching character stories:', error)
+      throw error
+    }
+  })
 
 export default {
   getCharacterDetail,
--- src/modules/characters/services/characters.ts
+++ src/modules/characters/services/characters.ts
@@ -1,31 +1,33 @@
 import axios from 'axios'
+import { useMemoize } from '@vueuse/core'
 
 import { getQueryParamsApi } from '@/modules/common/utils/query-params'
 
 const BASE_URL = 'https://gateway.marvel.com/v1/public'
 
-const getCharacters = async (offset: number, limit: number, nameStartsWith?:string | null, series?:string, comics?:string) => {
-  let url = `${BASE_URL}/characters?${getQueryParamsApi()}&limit=${limit}&offset=${offset}`
+const getCharacters = useMemoize(
+  async (offset: number, limit: number, nameStartsWith?: string | null, series?: string, comics?: string) => {
+    let url = `${BASE_URL}/characters?${getQueryParamsApi()}&limit=${limit}&offset=${offset}`
 
-  if (nameStartsWith) {
-    url = `${url}&nameStartsWith=${nameStartsWith}`
-  }
+    if (nameStartsWith) {
+      url = `${url}&nameStartsWith=${nameStartsWith}`
+    }
 
-  if (series) {
-    url = `${url}&series=${series}`
-  }
+    if (series) {
+      url = `${url}&series=${series}`
+    }
 
-  if (comics) {
-    url = `${url}&comics=${comics}`
-  }
+    if (comics) {
+      url = `${url}&comics=${comics}`
+    }
 
-  try {
-    const response = await axios.get(url)
-    return response.data.data
-  } catch (error) {
-    console.error('Error fetching characters:', error)
-    throw error
-  }
-}
+    try {
+      const response = await axios.get(url)
+      return response.data.data
+    } catch (error) {
+      console.error('Error fetching characters:', error)
+      throw error
+    }
+  })
 
 export default { getCharacters }
